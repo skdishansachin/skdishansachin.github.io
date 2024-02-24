8f1732a5a19adf708fa32fa9e5e7cbb7ce033ce1
skdishansachin <skdishansachin@gmail.com>
2024-02-25 00:01:25 +0530

    code refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dishansachin.vercel.app",
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    }
-  }),
-  integrations: [tailwind()]
+    site: 'https://dishansachin.vercel.app',
+    output: 'static',
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
+    integrations: [tailwind()],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -14,5 +14,5 @@ const postsCollection = defineCollection({
 });
 
 export const collections = {
-    'posts': postsCollection,
+    posts: postsCollection,
 };
