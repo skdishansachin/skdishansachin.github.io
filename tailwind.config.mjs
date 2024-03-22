26359ce7e2b6e14b80a2ad399555b088626dcfe2
skdishansachin <skdishansachin@gmail.com>
2024-03-22 22:12:46 +0530

    Add new fonts to package.json and pnpm-lock.yaml

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,8 +5,12 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {
+            colors: {
+                dark: '#0F0D0E',
+            },
             fontFamily: {
                 sans: ['Figtree Variable', ...defaultTheme.fontFamily.sans],
+                mono: ['Fira Mono', ...defaultTheme.fontFamily.mono],
             },
         },
     },
