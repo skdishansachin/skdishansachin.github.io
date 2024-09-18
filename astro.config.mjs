30a891b7003bb9741462392e3a479d45ae341462
skdishansachin <skdishansachin@gmail.com>
2024-09-18 11:59:53 +0530

    remove the blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config"
 import vercelStatic from "@astrojs/vercel/static"
 import tailwind from "@astrojs/tailwind"
-import sitemap from "@astrojs/sitemap"
 
 export default defineConfig({
     site: "https://dishansachin.vercel.app",
@@ -11,15 +10,5 @@ export default defineConfig({
             enabled: true,
         },
     }),
-    markdown: {
-        shikiConfig: {
-            theme: "github",
-            wrap: false,
-            themes: {
-                dark: "github-dark",
-                light: "github-light",
-            },
-        },
-    },
-    integrations: [tailwind(), sitemap()],
+    integrations: [tailwind()],
 })
--- src/utils/format.ts
+++
@@ -1,3 +0,0 @@
-export const { format: formatDate } = new Intl.DateTimeFormat('en-US', {
-    dateStyle: 'long',
-});
