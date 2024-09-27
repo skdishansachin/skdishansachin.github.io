685203da4d0037c3dc8ef0281651804e5e3192c6
skdishansachin <skdishansachin@gmail.com>
2024-09-27 11:19:38 +0530

    update the home page

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -1,33 +1,23 @@
 <template>
     <div class="space-y-6">
-        <NuxtImg
+        <!-- <NuxtImg
             src="/avatar.png"
             alt="Dishan Sachin"
             class="h-12 w-12 rounded-full border border-gray-300 bg-gray-200 ring-2 ring-gray-200 transition-all duration-300 hover:ring-4 sm:h-16 sm:w-16 dark:border-gray-800 dark:bg-gray-900 dark:ring-white/10"
             sizes="48px sm:64px"
             placeholder
             format="webp"
-        />
+        /> -->
         <h1
-            class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100"
+            class="text-xl font-semibold tracking-tight text-gray-800 dark:text-gray-100"
         >
-            Dishan Sachin
+            Hello, my name is Dishan Sachin.
         </h1>
         <p class="text-gray-900 dark:text-gray-400">
-            I'm Fayaz, I work as a software, product engineer and designer from
-            Bengaluru, India. I specialize in building web applications and
-            sites using Javascript, React, Vue &amp; Node. I've procrastinated
-            building this website for years but finally it's here, I've carved
-            out my own little nook on the internet to share my silly
-            experiments, nifty projects, and thoughts (mostly about tech and
-            design).
+            I am a software developer and also a casual writer. Most of the time, I'm doing web development and I really enjoy it. I'm a huge fan of open-source and I love to give back to the community. Right now, I'm studying for my BSc in Software Engineering at Java Institute for Advanced Technology.
         </p>
         <p class="text-gray-900 dark:text-gray-400">
-            By day, I'm a Fullstack Developer at
-            <a href="https://headshotpro.com" target="_blank" class="underline"
-                >Headshotpro</a
-            >, and by night (and weekends), I'm busy tinkering with some random
-            tool or app that I am building.
+            I like to work on different projects and, to be frank, most of them are not finished (I know you do too 😉). You can find some of my open-source projects I worked on on my GitHub profile.
         </p>
     </div>
 </template>
@@ -35,7 +25,6 @@
 <script setup>
 useSeoMeta({
     title: "Dishan Sachin",
-    description:
-        "I'm Fayaz, your friendly neighborhood software, product engineer and designer from Bengaluru, India. I specialize in building web applications and sites using Javascript, React, Vue & Node.",
+    description: "I am a software developer and also a casual writer. Most of the time, I'm doing web development and I really enjoy it. I'm a huge fan of open-source and I love to give back to the community. Right now, I'm studying for my BSc in Software Engineering at Java Institute for Advanced Technology.",
 })
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
                 class: "h-full",
             },
             bodyAttrs: {
-                class: "antialiased bg-gray-50 tracking-tight dark:bg-black min-h-screen",
+                class: "antialiased bg-gray-50 dark:bg-black min-h-screen",
             },
         },
     },
