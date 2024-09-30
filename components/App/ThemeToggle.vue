569ca1b11de430ba77d0e9bccec0dde220526aa8
skdishansachin <skdishansachin@gmail.com>
2024-09-30 10:35:47 +0530

    refactor the theme toggle changer

--- components/App/ThemeToggle.vue
+++ components/App/ThemeToggle.vue
@@ -12,17 +12,19 @@ const isDark = computed({
 </script>
 
 <template>
-    <UTooltip text="Toggle theme" :ui="{ popper: { strategy: 'absolute' } }">
-        <button
-            class="hover:text-primary-500 dark:hover:text-primary-400 relative flex items-center justify-center px-3 py-4 transition"
-            @click="isDark = !isDark"
-        >
-            <Icon
-                aria-hidden="true"
-                :name="isDark ? 'solar:sun-2-outline' : 'solar:moon-outline'"
-                class="h-5 w-5"
-            />
-            <span class="sr-only">Toggle theme</span>
-        </button>
-    </UTooltip>
+    <ClientOnly>
+        <UTooltip text="Toggle theme" :ui="{ popper: { strategy: 'absolute' } }">
+            <button
+                class="hover:text-primary-500 dark:hover:text-primary-400 relative flex items-center justify-center px-3 py-4 transition"
+                @click="isDark = !isDark"
+            >
+                <Icon
+                    aria-hidden="true"
+                    :name="isDark ? 'solar:sun-2-outline' : 'solar:moon-outline'"
+                    class="size-6"
+                />
+                <span class="sr-only">Toggle theme</span>
+            </button>
+        </UTooltip>
+    </ClientOnly>
 </template>
