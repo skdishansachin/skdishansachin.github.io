3d0eb2055aaa1397a7a665f1f01f48d91d16a3a2
skdishansachin <skdishansachin@gmail.com>
2025-02-05 09:10:23 +0530

    Add S3 bucket and CloudFront distribution for static site hosting

--- infra/lib/static-site-stack.ts
+++ infra/lib/static-site-stack.ts
@@ -1,9 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
+import { Distribution, OriginAccessIdentity, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class StaticSiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const bucket = new Bucket(this, 'Bucket', {
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity', {})
+
+    bucket.grantRead(originAccessIdentity);
+
+    const distribution = new Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new S3Origin(bucket, {
+          originAccessIdentity: originAccessIdentity,
+        }),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      }
+    });
   }
 }
