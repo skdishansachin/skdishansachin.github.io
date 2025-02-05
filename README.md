530f8d122d0999c962dcb80967f1051cb383e21e
skdishansachin <skdishansachin@gmail.com>
2025-02-05 07:46:47 +0530

    Remove files and folder

--- README.md
+++
@@ -1,3 +0,0 @@
-## Site
-
-My personal website, built with [Nuxt](https://nuxt.com/). You can view it [here](https://dishansachin.vercel.app).
--- components/App/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <footer
-        class="mx-auto max-w-2xl pb-8 text-center text-sm text-gray-400 dark:text-gray-600"
-    >
-        <br />
-        <p>© 2024 Dishan Sachin. All rights reserved.</p>
-    </footer>
-</template>
