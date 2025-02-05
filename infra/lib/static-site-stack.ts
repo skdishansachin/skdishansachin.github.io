5153441a3ad7ee4980ee4226fd9b554ff6aa78e0
skdishansachin <skdishansachin@gmail.com>
2025-02-05 11:00:38 +0530

    Enhance static site stack with improved S3 bucket configuration and CloudFront deployment

--- infra/lib/static-site-stack.ts
+++ infra/lib/static-site-stack.ts
@@ -1,30 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
+import { BlockPublicAccess, Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
 import { Distribution, OriginAccessIdentity, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
-import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import path = require('path');
 
 export class StaticSiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const bucket = new Bucket(this, 'Bucket', {
+      accessControl: BucketAccessControl.PRIVATE,
       autoDeleteObjects: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      enforceSSL: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
-
-    const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity', {})
-
-    bucket.grantRead(originAccessIdentity);
 
     const distribution = new Distribution(this, 'Distribution', {
       defaultRootObject: 'index.html',
       defaultBehavior: {
-        origin: new S3Origin(bucket, {
-          originAccessIdentity: originAccessIdentity,
-        }),
+        origin: S3BucketOrigin.withOriginAccessControl(bucket),
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       }
     });
+
+    new BucketDeployment(this, 'BucketDeployment', {
+      sources: [Source.asset(path.join(__dirname, '../../dist'))],
+      destinationBucket: bucket
+    });
+
+    new cdk.CfnOutput(this, 'CfnOutCloudFrontUrl', {
+      value: `https://${distribution.distributionDomainName}`,
+      description: "The CloudFront URL",
+    })
   }
 }
