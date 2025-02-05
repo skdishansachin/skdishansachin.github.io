2c0a45f31b617b93a990b5515f7b9f0688fd131e
skdishansachin <skdishansachin@gmail.com>
2025-02-05 08:12:57 +0530

    Rename infra stack to static site stack and update references

--- infra/bin/static-site.ts
+++ infra/bin/static-site.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { InfraStack } from '../lib/infra-stack';
+import { StaticSiteStack } from '../lib/static-site-stack';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
+new StaticSiteStack(app, 'StaticSiteStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
