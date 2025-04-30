0de66ea36161f0d69521c19d8ca28eab64c49556
skdishansachin <skdishansachin@gmail.com>
2025-04-30 21:31:11 +0530

    add @astrojs/cloudflare dependency to package.json

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
-	site: "https://example.com",
-	vite: {
-		plugins: [tailwindcss()],
+  site: "https://example.com",
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+  }),
 });
