2c84178aa3aa0f2e2feefd4f4ede18cc705b5796
skdishansachin <skdishansachin@gmail.com>
2025-08-06 09:42:12 +0530

    add optional description field

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,6 +4,7 @@ const blog = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
+    description: z.string().optional(),
     pubDate: z.coerce.date(),
     draft: z.boolean().default(false).optional(),
     tags: z.array(z.string()).optional(),
