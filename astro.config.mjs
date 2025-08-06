704974cb3195f69409e2a74cbc920c5d2f083bd5
skdishansachin <skdishansachin@gmail.com>
2025-08-06 09:46:06 +0530

    use the import alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import { SITE_URL } from "./src/consts";
+import { SITE_URL } from "@/src/consts";
 
 export default defineConfig({
   site: SITE_URL,
